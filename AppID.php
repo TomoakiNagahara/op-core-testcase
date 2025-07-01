98d2b939972734f0c5360f44cd423630e9c05b4c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-01 16:29:33 +0900

    New: AppID.php

---
+++ AppID.php
@@ -0,0 +1,63 @@
+<?php
+/**	op-core-testcase:/app_id.php
+ *
+ * 1. Check can get AppID
+ * 2. Check duplicate registration of AppID
+ *
+ * @created    2021-10-20
+ * @version    1.0
+ * @package    op-core
+ * @subpackage testcase
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+if( Env::AppID() !== OP::AppID() ){
+	Notice("OP::AppID() is broken.");
+}
+
+//	...
+$test = [];
+
+//	...
+try {
+	//	Get AppID.
+	$test[__LINE__] = Env::AppID();
+	//	Overwrite AppID.
+	$test[__LINE__] = Env::AppID('testcase');
+} catch ( \Throwable $e ){
+	//	...
+	$test[__LINE__] = $e->getMessage();
+
+	//	...
+	if( Notice::Has() ){
+		$test[__LINE__] = Notice::Get()['message'];
+	}else{
+		/* CI is in used.
+		Notice::Set("Feature of set AppID by argument will deprecated.");
+		*/
+	}
+
+	//	...
+	D($test);
+
+	//	...
+	return;
+}
+
+//	...
+D($test);
+
+//	...
+Notice("Please correct can duplicate registration of AppID.");
