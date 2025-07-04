8efd4500ce9cbc902f7d5c59068f5c0e43960490
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-04 16:18:24 +0900

    New: Request.php

---
+++ Request.php
@@ -0,0 +1,18 @@
+<?php
+/**	op-core-testcase:/Request.php
+ *
+ * @created    2025-07-04
+ * @version    1.0
+ * @package    op-core
+ * @subpackage testcase
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( OP::Request() );
