c8dfc3215ab1a35ae226f559bce81bd45b6f238f
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-13 03:55:48 +0900

    Add: RootPath.php | Test the detection of error in the asset root path.

--- RootPath.php
+++ RootPath.php
@@ -23,3 +23,12 @@ $doc = RootPath('doc');
 if( strpos( $app, $doc) !== 0 ){
 	OP::Error("The app root is not below the doc root: app={$app}, doc={$doc}");
 }
+
+//	Test the detection of error in the asset root path.
+require_once(_ROOT_CORE_.'/function/ConvertURL-2.php');
+D( ConvertURL(__DIR__) );
+$error = OP::Error()->Get();
+$message = $error['message'];
+if( $message !== 'This path is the asset root path: '.__DIR__ ){
+	OP::Error($message);
+}
