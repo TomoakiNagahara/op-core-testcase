19de8d72d1e8236afc568f298e6cc04dd56e543f
arch200G.uqunie.com <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-25 00:30:27 +0900

    New: mail.php | PHP mail function.

---
+++ mail.php
@@ -0,0 +1,51 @@
+<?php
+/**	op-core-testcase:/mail.php
+ *
+ * @created    2025-07-25
+ * @version    1.0
+ * @package    op-core
+ * @subpackage testcase
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	Check php.ini setting.
+if( $sendmail_path = `php -i | grep sendmail_path` ){
+	OP::Html($sendmail_path);
+}else{
+	OP::Error("sendmail_path is not set in php.ini.");
+}
+
+//	...
+$request = OP::Request();
+$to      = $request['to']      ??  null;
+$from    = $request['from']    ?? 'root';
+$subject = $request['subject'] ?? 'This is test mail';
+$message = $request['message'] ?? "This is test mail.";
+
+//	...
+if( empty($to) ){
+	//	...
+	$uid = fileowner(__FILE__);
+	$to  = posix_getpwuid($uid)['name'];
+}
+
+//	headers
+$headers  = "From: {$from}\r\n";
+$headers .= "Reply-To: noreply\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+//	...
+$io = mail($to, $subject, $message, $headers);
+echo 'mail: ' . $io ? 'Successful':'Failed';
+D($request);
