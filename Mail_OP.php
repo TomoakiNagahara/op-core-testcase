7b2d7adae91cfb77d2e2bd2df770229ac99ccd36
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-27 15:09:08 +0900

    New: Mail_OP.php

---
+++ Mail_OP.php
@@ -0,0 +1,49 @@
+<?php
+/**	op-core-testcase:/Mail_OP.php
+ *
+ * @created    2025-07-27
+ * @version    1.0
+ * @package    op-core
+ * @subpackage testcase
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+$request = OP::Request();
+$to      = $request['to']      ??  null;
+$subject = $request['subject'] ?? 'Test of HTML mail';
+$message = $request['message'] ?? "This is test mail.";
+
+//	...
+if( empty($to) ){
+	//	...
+	$uid = fileowner(__FILE__);
+	$to  = posix_getpwuid($uid)['name'];
+}
+
+//	...
+$headers = [
+	'cc'  => 'root',
+	'bcc' => 'root',
+];
+
+//	...
+$io = OP::Mail($to, $subject, $message, $headers);
+D($io);
+
+//	...
+$headers['mime'] = 'text/html';
+$message = "<h1>{$subject}</h1><p>{$message}</p>";
+$io = OP::Mail($to, $subject, $message, $headers);
+D($io);
