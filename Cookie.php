b1788ea79c1ceedcdb598cbefe7a45f1f374f7a5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-08-24 14:29:38 +0900

    New: Cookie.php

---
+++ Cookie.php
@@ -0,0 +1,48 @@
+<?php
+/**	op-core:/testcase/cookie.php
+ *
+ * @created   2021-05-15
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/*  @var $init boolean */
+$user_id = Cookie::UserID($init);
+
+//	...
+$count = Cookie::Get('count', 0);
+$count++;
+Cookie::Set('count', $count);
+
+?>
+<section class="markdown" data-translation="true">
+The "Cookie" class
+===
+
+ The "Cookie" class is easy to save and get values from the "Cookie".
+ The values saved by the "Cookie" class are encrypted. So can not be view and update on the user side.
+
+```php
+namespace OP;
+Cookie::Set('foo', 'bar');
+echo Cookie::Get('foo');
+```
+
+ The "Cookie" class can generate a unique ID.
+
+```php
+echo Cookie::UserID();
+```
+</section>
+
+<section>
+	<p>UserID : <?= $user_id ?> (Initialization? <?= $init ? 'true':'false' ?>)</p>
+	<p>Count up : <?= $count ?></p>
+</section>
